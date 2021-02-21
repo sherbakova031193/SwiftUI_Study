382a9012be357ecc265825d8c0046fd2bd6ce6d7
Елизавeта <sherbakova031193@gmail.com>
2021-02-21 13:19:11 +0300

    Initial Commit

---
+++ SwiftUI_Study/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Study
+//
+//  Created by Елизавета Щербакова on 21.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
